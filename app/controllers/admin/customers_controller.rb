7eeffb7216b9d11fca883136b4ddd303991e1572
a.yoshiyuki <a.yoshiyuki3285@gmail.com>
2021-02-04 05:07:04 +0000

    [Start]admin側　各アクション＆viewファイル作成開始

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,35 @@
 class Admin::CustomersController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def list
+
+  end
+
+  def exit
+
+  end
+
 end
