96820c1b7941de113df69a3df0c200db4a369f80
a.yoshiyuki <a.yoshiyuki3285@gmail.com>
2021-02-06 02:08:19 +0000

    [Start]admin側　利用者新規登録画面

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -17,11 +17,13 @@ class Admin::CustomersController < ApplicationController
   end
 
   def new
-
+    @customer = Customer.new
   end
 
   def create
-
+    @customer = Customer.new(customer_params)
+    @customer.save
+    redirect_to admin_customers_path(@customer)
   end
 
   def list
