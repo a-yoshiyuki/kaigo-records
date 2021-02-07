edd2d77ec064e465e44cc01030d3b3347f65e62a
a.yoshiyuki <a.yoshiyuki3285@gmail.com>
2021-02-07 14:09:14 +0000

    [Start]admin側　在籍ステータス　実装

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -40,6 +40,6 @@ class Admin::CustomersController < ApplicationController
 
   private
     def customer_params
-      params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana)
+      params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :is_deleted)
     end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,9 @@ class Customer < ApplicationRecord
 
   has_many :progress_record
   has_many :vital_record
+  has_many :meal_record
+
+  enum is_deleted: { 利用中: 0, 入院中: 1, 退所: 3 }
 
   def full_name
     self.first_name + "　" + self.last_name
