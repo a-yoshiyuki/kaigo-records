a66fcdd06b073b9c1d05c90d886a88914b30b5f7
a.yoshiyuki <a.yoshiyuki3285@gmail.com>
2021-02-08 13:55:15 +0000

    [Start]staff側　個人記録詳細画面

--- app/controllers/staff_member/customers_controller.rb
+++ app/controllers/staff_member/customers_controller.rb
@@ -6,6 +6,12 @@ class StaffMember::CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
+
+  private
+    def customer_params
+      params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :is_deleted)
+    end
 
 end
