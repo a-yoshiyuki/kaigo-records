f58be43b28bf99941c8fc64bb00066aa91b3a156
a.yoshiyuki <a.yoshiyuki3285@gmail.com>
2021-02-18 07:05:32 +0000

    [Add]staff側　経過記録新規入力機能作成

--- app/controllers/staff_member/progress_records_controller.rb
+++ app/controllers/staff_member/progress_records_controller.rb
@@ -2,18 +2,25 @@ class StaffMember::ProgressRecordsController < ApplicationController
 
   def show
     @customer = Customer.find(params[:id])
+    @progress_records = @customer.progress_records.where(updated_at: Time.zone.today.beginning_of_day..Time.zone.today.end_of_day).order(id: "DESC")
   end
 
   def new
+    @progress_record = ProgressRecord.new
   end
+
+  def create
+    @progress_record = ProgressRecord.new(progress_record_params)
+    @progress_record.save
+    @customer = Customer.find(progress_record_params[:customer_id])
+    redirect_to staff_member_progress_record_path(@customer.id)
+  end
+
 
   def edit
   end
 
   def index
-  end
-
-  def create
   end
 
   def update
@@ -24,6 +31,6 @@ class StaffMember::ProgressRecordsController < ApplicationController
 
   private
     def progress_record_params
-      params.require(:progress_record).(:customer_id, :staff_member_id, :time, :detail)
+      params.require(:progress_record).permit(:customer_id, :staff_member_id, :time, :detail)
     end
 end
