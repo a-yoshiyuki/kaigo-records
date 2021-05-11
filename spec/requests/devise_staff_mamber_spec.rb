dff0f9d1b0152423f1a170e319904d91fdc75342
a.yoshiyuki <a.yoshiyuki3285@gmail.com>
2021-05-11 13:18:11 +0000

    [Add]RSpec requestsテスト devise_staff_member_spec.rb 追加

--- spec/requests/devise_staff_mamber_spec.rb
+++ spec/requests/devise_staff_mamber_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe "admin_devise_sign_in", type: :request do
-  let(:admin) {create(:admin)}
-  let(:admin_params) { attributes_for(:admin) }
-  let(:invalid_admin_params) { attributes_for(:admin, name: "") }
+RSpec.describe "staff_member_devise_sign_in", type: :request do
+  let(:staff_member) {create(:staff_member)}
+  let(:staff_member_params) { attributes_for(:staff_member) }
+  let(:invalid_staff_member_params) { attributes_for(:staff_member, name: "") }
 
   describe 'GET #new' do
     context "ログイン画面が正しく表示される" do
       before do
-        get new_admin_session_path
+        get new_staff_member_session_path
       end
       it 'リクエストは200 OKとなること' do
         expect(response.status).to eq 200
@@ -17,7 +17,7 @@ RSpec.describe "admin_devise_sign_in", type: :request do
 
     context "新規登録画面が正しく表示される" do
       before do
-        get new_admin_registration_path
+        get new_staff_member_registration_path
       end
       it 'リクエストは200 OKとなること' do
         expect(response.status).to eq 200
@@ -32,15 +32,15 @@ RSpec.describe "admin_devise_sign_in", type: :request do
     end
     context 'パラメータが妥当な場合' do
       it 'リクエストが成功すること' do
-        post admin_registration_path, params: { admin: admin_params }
+        post staff_member_registration_path, params: { staff_member: staff_member_params }
         expect(response.status).to eq 302
       end
     end
 
     it 'createが成功すること' do
       expect do
-        post admin_registration_path, params: { admin: admin_params }
-      end.to change(Admin, :count).by 1
+        post staff_member_registration_path, params: { staff_member: staff_member_params }
+      end.to change(StaffMember, :count).by 1
     end
   end
 end
