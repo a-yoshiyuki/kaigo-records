608b9a47f84b24fe4001ae12e50f5e4a5e1a9b64
a.yoshiyuki <a.yoshiyuki3285@gmail.com>
2021-05-17 14:35:46 +0000

    [Add]RSpec requests 2フォルダ追加

---
+++ spec/requests/StaffMember::VitalRecordsController_requests_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe "StaffMember::VitalRecordsController", type: :request do
+  let!(:staff_member) { FactoryBot.create(:staff_member) }
+  let!(:customer) { FactoryBot.create(:customer) }
+  let!(:vital_record) { FactoryBot.create(:vital_record) }
+  before do
+    sign_in staff_member
+  end
+
+    describe 'バイタル記録詳細ページ' do
+    context "バイタル記録詳細ページが正しく表示される" do
+      before do
+        get staff_member_vital_record_path(customer.id)
+      end
+      it 'リクエストは200 OKとなること' do
+        expect(response.status).to eq 200
+      end
+    end
+  end
+
+  describe '過去のバイタル記録ページ' do
+    context "過去のバイタル記録ページが正しく表示される" do
+      before do
+        get staff_member_index_staff_member_vital_record_path(customer.id)
+      end
+      it 'リクエストは200 OKとなること' do
+        expect(response.status).to eq 200
+      end
+    end
+  end
+
+  describe 'バイタル記録入力ページ' do
+    context "バイタル記録入力ページが正しく表示される" do
+      before do
+        get new_staff_member_vital_record_path
+      end
+      it 'リクエストは200 OKとなること' do
+        expect(response.status).to eq 200
+      end
+    end
+  end
+
+  describe 'バイタル記録編集ページ' do
+    context "バイタル記録編集ページが正しく表示される" do
+      before do
+        get edit_staff_member_vital_record_path(vital_record.id)
+      end
+      it 'リクエストは200 OKとなること' do
+        expect(response.status).to eq 200
+      end
+    end
+  end
+end
